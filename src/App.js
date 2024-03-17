29df8cdacebcf7027766e469d43802157e52fc26
Alexander_Ovchar <azalexxx02@gmail.com>
2024-03-17 19:55:05 +0200

    init v2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Error from "./pages/Error";
-import Header from "./components/Header/Header";
-import { useTranslation } from "react-i18next";
-import Footer from "./components/Footer/Footer";
-
 function App() {
-  const { t, i18n } = useTranslation();
-
-  return (
-    <div className="app ">
-      <div className="max-w-screen-xl w-full mx-auto ">
-        <div className="fixed w-[100vw] h-[100vw] max-w-[1440px] max-h-[1440px]  pointer-events-none flex justify-center items-center opacity-20  dark:opacity-5">
-          <img className="w-1/2" src="/logo.svg" alt="" />
-        </div>
-        <Header t={t} i18n={i18n} />
-        <Routes>
-          <Route path="/" element={<Home t={t} i18n={i18n} />}></Route>
-          <Route path="*" element={<Error t={t} i18n={i18n} />}></Route>
-        </Routes>
-        <Footer t={t} i18n={i18n} />
-      </div>
-    </div>
-  );
+  return <div className="app ">123</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import './i18next'
-import Loader from './components/Loader/Loader';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Suspense fallback={(<Loader />)}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Suspense>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
-
-
