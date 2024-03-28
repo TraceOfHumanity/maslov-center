503538a1cbe8d2622778eb832d53005eca47990e
Alexander_Ovchar <azalexxx02@gmail.com>
2024-03-28 02:16:12 +0200

    fix bags in banner for mobile

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,49 +12,51 @@ export const Banner = () => {
   const textContainerRef = useRef(null);
 
   useEffect(() => {
-    gsap.to(particlesContainerRef.current, {
-      duration: 2,
-      opacity: 1,
-    });
-
+    const mm = gsap.matchMedia();
     const textContainer = textContainerRef.current;
     const h1 = textContainer.querySelector('h1');
     const h4 = textContainer.querySelector('h4');
     const a = textContainer.querySelector('a');
 
-    gsap.fromTo(
-      h1,
-      { y: 50, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 2,
-        delay: 0.3,
-        ease: 'elastic.inOut(0.5, 0.7) ',
-      },
-    );
-    gsap.fromTo(
-      h4,
-      { y: 50, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 2,
-        delay: 0.5,
-        ease: 'elastic.inOut(0.5, 0.7) ',
-      },
-    );
-    gsap.fromTo(
-      a,
-      { y: 50, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 2,
-        delay: 0.7,
-        ease: 'elastic.inOut(0.5, 0.7) ',
-      },
-    );
+    gsap.to(particlesContainerRef.current, {
+      duration: 2,
+      opacity: 1,
+    });
+    mm.add('(min-width: 768px)', () => {
+      gsap.fromTo(
+        h1,
+        { y: 50, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 2,
+          delay: 0.3,
+          ease: 'elastic.inOut(0.5, 0.7) ',
+        },
+      );
+      gsap.fromTo(
+        h4,
+        { y: 50, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 2,
+          delay: 0.5,
+          ease: 'elastic.inOut(0.5, 0.7) ',
+        },
+      );
+      gsap.fromTo(
+        a,
+        { y: 50, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 2,
+          delay: 0.7,
+          ease: 'elastic.inOut(0.5, 0.7) ',
+        },
+      );
+    });
   }, []);
 
   return (
@@ -77,8 +79,8 @@ export const Banner = () => {
         className="flex flex-col items-center justify-center gap-2 pt-[5vw] pb-[5vw] md:gap-3 lg:gap-4"
         ref={textContainerRef}
       >
-        <h1 className="text-center opacity-0">{t('banner.title')}</h1>
-        <h4 className="max-w-3xl text-center opacity-0">
+        <h1 className="text-center md:opacity-0">{t('banner.title')}</h1>
+        <h4 className="max-w-3xl text-center md:opacity-0">
           {t('banner.subtitle')}
         </h4>
       </div>
