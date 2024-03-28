2ee8e4277c6a03b09631ece4b3976a8b4d1b6a9d
Alexander_Ovchar <azalexxx02@gmail.com>
2024-03-28 02:06:36 +0200

    add overflow-hidden for main container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import { useTranslation } from 'react-i18next';
-
 import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
 import { Main } from 'components/Main';
 
 function App() {
   return (
-    <div className="container flex min-h-screen flex-col gap-5">
+    <div className="container flex min-h-screen flex-col gap-5 overflow-hidden">
       <Header />
       <Main />
       <Footer />
