71999f8033e3eae2f62802e9061e930c44ee34b4
Alexander_Ovchar <azalexxx02@gmail.com>
2025-05-15 20:14:28 +0300

    Refactor code style and update dependencies
    - Standardized import statements in various files to use double quotes.
    - Updated ESLint configuration for improved code quality.
    - Added new dependencies for internationalization (i18next) and code formatting (prettier).
    - Enhanced the App component structure by integrating multiple components (Header, Banner, Benefits, Services, Gallery, Footer).
    - Updated CSS for a gradient background.
    - Removed old logo SVG files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { Banner } from "./components/banner";
+import { Benefits } from "./components/benefits";
+import { Footer } from "./components/footer";
+import { Gallery } from "./components/gallery";
+import { Header } from "./components/header";
+import { Services } from "./components/services";
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    <div className="container mx-auto">
+      <Header />
+      <Banner />
+      <Benefits />
+      <Services />
+      <Gallery />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
