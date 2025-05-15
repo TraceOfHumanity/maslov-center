89aacfa8d3bab1e5ae316ccc924b77cec8c95ab5
Alexander_Ovchar <azalexxx02@gmail.com>
2025-05-15 21:47:58 +0300

    add dark mode

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,12 @@
+import { useTheme } from "@/hooks/useTheme";
+import { useAppSelector } from "@/redux/hooks";
+
 export const Header = () => {
-  return <div>Header</div>;
+  const { theme } = useAppSelector((state) => state.theme);
+  const { handleTheme } = useTheme();
+  return (
+    <div>
+      <button onClick={handleTheme}>{theme === "light" ? "Light" : "Dark"}</button>
+    </div>
+  );
 };
