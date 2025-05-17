a06774541ef534fde83b506effdd66ac257b19e4
Alexander_Ovchar <azalexxx02@gmail.com>
2025-05-18 00:44:17 +0300

    style: update website background with image and blur effect

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -2,11 +2,11 @@ import { useTranslation } from "react-i18next";
 
 export const Banner = () => {
   const { t } = useTranslation();
-  return <div>
-    <svg className="w-80 h-80 mx-auto">
-      <use href="/logo-with-label.svg#logo-with-label" />
-    </svg>
-    <h1 className="text-center text-4xl font-bold mb-8">{t('banner.title')}</h1>
+  return <div className="flex flex-col items-center justify-center gap-[1vw]">
+    <div className="w-80 mx-auto">
+      <img src="/logo-with-label.svg" alt="logo" />
+    </div>
+    <h1 className="text-center text-4xl font-bold">{t('banner.title')}</h1>
     <p className="max-w-3xl text-center mx-auto">
       {t('banner.subtitle')}
     </p>
