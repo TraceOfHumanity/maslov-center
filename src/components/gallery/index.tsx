878ab0e679cc5fd2110c91e9b0fe39899dab3a87
Alexander_Ovchar <azalexxx02@gmail.com>
2025-05-18 19:57:14 +0300

    refactor: improve image and video presentation in Gallery component
    - Wrapped image and video elements in a div with a square aspect ratio for better layout consistency.
    - Updated image class to use 'object-cover' for improved visual presentation.
    - Ensured that the Gallery component maintains a responsive design for various media types.

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -22,25 +22,28 @@ export const Gallery = () => {
       <CarouselContent>
         {sliderItems.map((item) => (
           <CarouselItem key={item.id} className="basis-1/3 aspect-video">
-            {item.img ? (
-              <img
-                key={item.id}
-                className="w-full h-fill object-fill"
-                src={item.img}
-                alt={item.alt}
-                loading="lazy"
-                crossOrigin="anonymous"
-              />
-            ) : (
-              <iframe
-                className="z-0 h-full w-full"
-                src={item.video}
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
-                allowFullScreen
-              ></iframe>
-            )}
+            <div className="w-full aspect-square">
+
+              {item.img ? (
+                <img
+                  key={item.id}
+                  className="w-full h-full object-cover"
+                  src={item.img}
+                  alt={item.alt}
+                  loading="lazy"
+                  crossOrigin="anonymous"
+                />
+              ) : (
+                <iframe
+                  className="z-0 h-full w-full"
+                  src={item.video}
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen
+                ></iframe>
+              )}
+            </div>
           </CarouselItem>
         ))}
       </CarouselContent>
